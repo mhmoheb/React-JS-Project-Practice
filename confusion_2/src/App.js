2f46ccd2f0e5d61d0b810d816f1b3941ea6829d9
mhmoheb <moheb5159@gmail.com>
2022-08-16 16:47:18 -0500

    presentational and container components

---
+++ confusion_2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Main from './components/MainComponent';
+import { Component } from 'react';
+
+class App extends Component {
+  
+  render(){
+    return (
+      <div className="App">
+        <Main />
+      </div>
+      
+    );
+  }
+}
+export default App;
---
+++ confusion_2/src/components/DishdetailComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+
+    renderDish(dish){
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+             );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(dish) { 
+
+        if (dish != null) { 
+
+            const comments = dish.comments.map((comment) => { 
+                return ( 
+                    <div key={comment.id}> 
+                        <ul>{comment.comment}</ul> 
+                        <ul>--{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(comment.date.toLocaleDateString)} </ul> 
+                    </div> 
+                ); 
+            }); 
+
+            return ( 
+                <div> 
+                    <h4>Comments:</h4> 
+                    {comments} 
+                </div> 
+            ); 
+        } 
+        else 
+            return ( 
+                <div></div> 
+            );
+    } 
+    render() {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.dish)}
+                </div>
+            </div>
+           
+        );
+    }
+
+}
+
+
+
+export default DishDetail;
+
+
